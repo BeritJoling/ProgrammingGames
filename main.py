5ddd475d89473cca796e11d600f2719db6fef1f9
BeritJoling <berit.joling@student.aventus.nl>
2023-03-22 09:23:33 +0100

    Added main.py for the main menu (WIP)

---
+++ main.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter import ttk
+
+def game1():
+    pass
+
+def game2():
+    pass
+
+def game3():
+    pass
+
+def game4():
+    pass
+
+def game5():
+    pass
+
+
+
+
+window = tk.Tk()
+window.title("Main Menu")
+
+
